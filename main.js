3608fe0141e292ad4f5929f276d33a82f486a96b
Skye Sprung <skye.sprung@gmail.com>
2021-06-09 13:11:58 +0200

    added socket dep and login get

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const express = require('express')
+const socket = require('socket.io')
 const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const io = socket(server)
 
-app.get('/', (req, res) =>{
-    console.log(req.path)
-    res.sendFile(__dirname + "/index.html")
 
-})
-
-app.get("/favicon.ico", (req, res) => res.sendFile(__dirname + "/resources/favicon.ico"))
+app.get('/', (req, res) =>
+    res.sendFile(__dirname + "/index.html"))
+app.get("/login", (req, res) =>
+    res.sendFile(__dirname + "/login.html"))
+app.get("/favicon.ico", (req, res) =>
+    res.sendFile(__dirname + "/resources/favicon.ico"))
 app.get("/chat/*", (req, res) => {
 })
-app.get("/bootstrap.css", (req, res) => res.sendFile(__dirname + "/resources/bootstrap.min.css"))
-app.get("/style.css", (req, res) => res.sendFile(__dirname+"/style.css"))
-app.listen(3000, ()=>console.log("listening on port 3000"))
+app.get("/bootstrap.css", (req, res) =>
+    res.sendFile(__dirname + "/resources/bootstrap.min.css"))
+app.get("/style.css", (req, res) =>
+    res.sendFile(__dirname + "/style.css"))
+server.listen(3000, () => console.log("listening on port 3000"))
+
+
+io.on('connect', (s) => {
+    console.log("connection")
+})
