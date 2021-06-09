414d9c19e5c03f94be812deab40b1a5aa4a3a1e4
Skye Sprung <skye.sprung@gmail.com>
2021-06-09 15:22:31 +0200

    added mongo and socket functionality

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 const io = socket(server)
+const mongoclient = require('mongodb').MongoClient
+const url  = "mongodb://127.0.0.1:27017"
+
+
 
 
 app.get('/', (req, res) =>
@@ -18,9 +22,22 @@ app.get("/bootstrap.css", (req, res) =>
     res.sendFile(__dirname + "/resources/bootstrap.min.css"))
 app.get("/style.css", (req, res) =>
     res.sendFile(__dirname + "/style.css"))
+app.get("/register", (req, res) => res.sendFile("register.html"))
 server.listen(3000, () => console.log("listening on port 3000"))
 
-
-io.on('connect', (s) => {
-    console.log("connection")
+mongoclient.connect(url, (err,client) =>{
+    if(err) throw err
+    var db = client.db("users")
+    io.on('connect', (s) => {
+        console.log("connection")
+        s.on("checkuser", (data) =>{
+            db.collection("users").findOne({"user-id":data.user}).then((datael) =>{
+                if(datael == null || datael.hash != data.hash){
+                    //user does not exist
+                    io.to(s.id).emit("nouser")
+                }
+            }, (x) =>{
+            })
+        })
+    })
 })
